9933f8b902a0f7f50dfc1c984adc0a095877fcbd
Younes Noufair <48910083+UNES97@users.noreply.github.com>
2023-05-04 17:27:33 +0200

    Test

--- test.php
+++ test.php
@@ -4,14 +4,14 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$host = "51.195.41.55";
-$dbname = "macturama_stable";
-$password = "StableM@ctura332";
-$username = "macturama_stable";
+$host = "51.68.174.244";
+$dbname = "maxmind_remote_db";
+$password = "6ALUIHShCzSx";
+$username = "maxmind_remote_user";
 
 $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $pdo->prepare("SELECT * FROM tblclients");
+$stmt = $pdo->prepare("SELECT * FROM tblusers");
 $stmt->execute();
 $result = $stmt->fetchAll();
 print_r($result);
