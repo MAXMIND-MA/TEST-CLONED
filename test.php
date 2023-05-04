72b91d09f2aa8fff8cc2ad69bbc85244399b84bf
Younes Noufair <48910083+UNES97@users.noreply.github.com>
2023-05-04 17:22:49 +0200

    Test DB Connection

--- test.php
+++ test.php
@@ -1,3 +1,13 @@
 <?php 
-//echo "MAXMIND";
-phpinfo();
+
+$host = "51.195.41.55";
+$dbname = "macturama_stable";
+$password = "StableM@ctura332";
+$username = "macturama_stable";
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+$stmt = $pdo->prepare("SELECT * FROM tblclients");
+$stmt->execute();
+$result = $stmt->fetchAll();
+print_r($result);
