6633264c73ab11838a5b607d14a72df76ed9d4e8
Younes Noufair <48910083+UNES97@users.noreply.github.com>
2023-05-05 16:41:14 +0100

    Update test.php

--- test.php
+++ test.php
@@ -4,18 +4,14 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$host = "10.11.227.36";
+$host = "localhost";
 $dbname = "maxmind_db";
+$password = "Maxmind@123";
 $username = "maxmind_user";
-$password = "M@xmind@2023.";
 
-try {
-    $pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname", $username, $password);
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $pdo->prepare("SELECT * FROM tblusers");
+$stmt = $pdo->prepare("SELECT * FROM users");
 $stmt->execute();
 $result = $stmt->fetchAll();
 print_r($result);
